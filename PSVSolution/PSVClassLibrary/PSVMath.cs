5637236b6626c6cbd1271b21fc1659cf96abdcad
Sergey.Prokushev@hotmail.com <Sergey.Prokushev@hotmail.com>
2019-12-24 11:37:59 +0500

    1

---
+++ PSVSolution/PSVClassLibrary/PSVMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace PSVClassLibrary
+{
+    public class PSVMath
+    {
+
+        // Факториал
+        public static BigInteger Factorial (int number)
+        {
+            BigInteger result = 1;
+            
+            for (int i = number; i > 0; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+
+
+    }
+}
---
+++ PSVSolution/TestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PSVClassLibrary;
+
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(RomanNumeric.TryParse("XCIX",out _));
+           Console.WriteLine(PSVClassLibrary.PSVMath.Factorial(5));
+        }
+    }
+}
